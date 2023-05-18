9bebed9b40e90a6ae9ba1bd60b0233f01b6e08ec
UltimaSL <alexandercamargo787@gmail.com>
2023-05-17 22:42:40 -0500

    Nueva version del programa
    una version mas simplificada y corta del problema para atrapar los errores con un solo "try and catch"

---
+++ taller2menuv2.java
@@ -0,0 +1,87 @@
+import java.io.*;
+import java.text.DecimalFormat;
+
+//en repositorio de github
+public class taller2menuv2 {
+    public static void main(String[] args) throws Exception {
+
+        boolean ciclo1 = true;
+        float lado1 = 0;
+        float lado2 = 0;
+        float lado3 = 0;
+        double perimetro;
+        double area;
+
+        DecimalFormat format1 = new DecimalFormat("#.00");
+        BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
+
+        while (ciclo1) {
+                Float option = valor("que figura va a calcular?\n1.Triangulo\n2.Ciculo\n3.rectangulo\n4.Salir del programa\n");
+
+                if (option == 1) // Calculos del p y a del triangulo
+                {
+                    //lectura de variables
+                    lado1 = valor("Introduzca el lado 1");
+                    lado2 = valor("Introduzca el lado 2");
+                    lado3 = valor("Introduzca el lado 3");
+
+                    //operaciones arimeticas 
+                    perimetro = lado1 + lado2 + lado3;
+                    area = Math.sqrt((perimetro / 2) * ((perimetro / 2) - lado1) * ((perimetro / 2) - lado2)* ((perimetro / 2) - lado3));
+
+                    System.out.println("\n\nEl perimetro es: " + format1.format(perimetro) + "\nEl area es: " + format1.format(area)+"\n\n");
+
+                }
+
+                else if (option == 2) // Calculos del p y a del circulo
+                {
+                    //lectura de variables
+                    lado1 = valor("Introduzca el radio");
+                    //operaciones aritmeticas
+                    perimetro = (2 * lado1) * Math.PI;
+                    area = Math.PI * (lado1 * lado1);
+
+                    System.out.println("\n\nEl perimetro es: " + format1.format(perimetro) + "\nEl area es: " + format1.format(area)+"\n\n");
+
+                } else if (option == 3) // Calculos del p y a del rectangulo
+                {
+                    //lectura de variables
+                    lado1 = valor("Introduzca la base");
+                    System.out.println("");
+                    lado2 = valor("Introduzca la altura");
+
+                    //operaciones aritmeticas
+                    perimetro = lado1 * 2 + lado2 * 2;
+                    area = lado1 * lado2;
+
+                    System.out.println("\n\nEl perimetro es: " + format1.format(perimetro) + "\nEl area es: " + format1.format(area)+"\n\n");
+
+                }
+
+                else if (option==4) {//salir del programa
+                    System.out.println("\n\nSi desea salir del programa?\nIntroduzca EXIT\nCaso contrario pulse cualquier tecla\n\n");
+                    String option2=leer.readLine();
+                    if(option2.equals("EXIT")){
+                    ciclo1=false;  
+                    }
+                }
+
+            } 
+        }
+
+    public static float valor(String mensaje) {//funcio para errores, solo se admiten numeros reales
+
+        BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            System.out.println(mensaje);
+            return Float.parseFloat(leer.readLine());
+        } catch (Exception e) {
+            System.out.println("\nIngrese un valor valido\n");
+            return valor(mensaje);
+        }
+
+    }
+
+}
+    
+
